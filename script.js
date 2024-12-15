c7f75bca2ee91851a0ec96adcc26364160f0eb42
Double A <axror1rorxa@gmail.com>
2024-12-15 23:10:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const greeting = document.createElement('p');
-    greeting.textContent = "Hello! Thanks for visiting our website. This is the Official Website of Jahonkash TV";
+    greeting.textContent = "Hello! Thanks for visiting our website. This is the Official Website of Jahonkash TV.";
     document.querySelector('main').appendChild(greeting);
 });
